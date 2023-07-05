72febf9f0f8f787fe45cd9dd10c0c2e09616cd61
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-07-05 11:49:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+Clear();
+WriteLine();
+int[,] arrr = new int[5, 9];
+FilArray(arrr);
+PrintArray(arrr);
+SortArray(arrr);
+PrintArray(arrr);
+
+void FilArray(int[,] ar)
+{
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++) ar[i, j] = new Random().Next(1, 99);
+    }
+}
+
+
+void PrintArray(int[,] ar)
+{
+    for (int i = 0; i < ar.GetLength(0); i++)
+    {
+        for (int j = 0; j < ar.GetLength(1); j++) Write(ar[i, j] + " ");
+        WriteLine();
+    }
+    WriteLine();
+}
+
+
+void SortArray(int[,] ar)
+{
+    for (int k = 0; k < ar.GetLength(0); k++)
+    {
+        for (int j = 0; j < ar.GetLength(1) - 1; j++)
+        {
+            for (int i = 0; i < ar.GetLength(1) - 1; i++)
+            {
+
+                if (ar[k, i] < ar[k, i + 1])
+                {
+                    int c = ar[k, i];
+                    ar[k, i] = ar[k, i + 1];
+                    ar[k, i + 1] = c;
+
+                }
+
+            }
+        }
+    }
+    return;
+}
